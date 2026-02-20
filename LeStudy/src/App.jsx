601d7421633cc21e673c2ab717bd7f0e9d686641
Patrick <pmartinez2972@yahoo.com>
2026-02-20 14:23:10 -0600

    Initial setup for the site

---
+++ LeStudy/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className=''>
+      <h1>test</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
